be173f648cc1401f7022c4a6c7ad57f6885f6337
tenfy <tenfy@tenfy.cn>
2023-01-28 13:04:58 +0800

    abbreviate

---
+++ lua/abbreviate.lua
@@ -0,0 +1,43 @@
+--[[
+- @file abbreviate.lua
+- @brief  
+- @author tenfyzhong
+- @email tenfy@tenfy.cn
+- @created 2023-01-28 13:02:49
+--]]
+
+vim.cmd([[
+iab eles else
+iab contineu continue
+iab 1= !=
+
+augroup ft_abbreviate
+  au!
+  autocmd FileType python iab <buffer> true True
+  autocmd FileType python iab <buffer> false False
+  autocmd FileType python iab <buffer> none None
+  autocmd FileType python iab <buffer> __MAIN__ __main__
+  autocmd FileType python iab <buffer> __NAME__ __name__
+
+  autocmd FileType proto iab <buffer> int int32
+  autocmd FileType proto iab <buffer> uint uint32
+  autocmd FileType proto iab <buffer> byte bytes
+  autocmd FileType proto iab <buffer> return returns
+  autocmd FileType proto iab <buffer> i16 int16
+  autocmd FileType proto iab <buffer> i32 int32
+  autocmd FileType proto iab <buffer> i64 int64
+
+  autocmd FileType go iab <buffer> ;= :=
+
+  autocmd FileType lua iab <buffer> != ~=
+  autocmd FileType lua iab <buffer> 1= ~=
+
+  autocmd FileType nginx iab <buffer> != ~=
+  autocmd FileType nginx iab <buffer> 1= ~=
+
+  autocmd FileType thrift iab <buffer> int16 i16
+  autocmd FileType thrift iab <buffer> int32 i32
+  autocmd FileType thrift iab <buffer> int64 i64
+  autocmd FileType thrift iab <buffer> import include
+augroup end
+]])
