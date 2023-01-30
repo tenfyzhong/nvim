3545de4acb74a8e51af04c2b20bda06573ca2d38
tenfy <tenfy@tenfy.cn>
2023-01-30 13:40:38 +0800

    GoGet command

---
+++ lua/command.lua
@@ -0,0 +1,10 @@
+--[[
+- @file command.lua
+- @brief  
+- @author tenfyzhong
+- @email tenfy@tenfy.cn
+- @created 2023-01-30 13:23:00
+--]]
+local feature = require('feature')
+
+feature.GoGetCommand()
