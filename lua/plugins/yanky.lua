402e251ce6842b978c109668d34ee569887e9686
tenfy <tenfy@tenfy.cn>
2023-02-03 21:24:53 +0800

    yanky plugin

---
+++ lua/plugins/yanky.lua
@@ -0,0 +1,22 @@
+--[[
+- @file yanky.lua
+- @brief  
+- @author tenfyzhong
+- @email tenfy@tenfy.cn
+- @created 2023-02-03 12:04:27
+--]]
+
+local yanky = {
+    'gbprod/yanky.nvim',
+    config = function()
+        require("yanky").setup {}
+        vim.keymap.set({ "n", "x" }, "p", "<Plug>(YankyPutAfter)")
+        vim.keymap.set({ "n", "x" }, "P", "<Plug>(YankyPutBefore)")
+        vim.keymap.set({ "n", "x" }, "gp", "<Plug>(YankyGPutAfter)")
+        vim.keymap.set({ "n", "x" }, "gP", "<Plug>(YankyGPutBefore)")
+        vim.keymap.set("n", "'yn", "<Plug>(YankyCycleForward)")
+        vim.keymap.set("n", "'yp", "<Plug>(YankyCycleBackward)")
+    end,
+}
+
+return { yanky }
