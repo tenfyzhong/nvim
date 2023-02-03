81519899b9f26f19abbcd4e6784900558b6aecec
tenfy <tenfy@tenfy.cn>
2023-02-03 21:23:45 +0800

    fold setting

---
+++ lua/plugins/nvim-ufo.lua
@@ -0,0 +1,38 @@
+--[[
+- @file nvim-ufo.lua
+- @brief  
+- @author tenfyzhong
+- @email tenfy@tenfy.cn
+- @created 2023-02-03 15:24:32
+--]]
+local statuscol = {
+    "luukvbaal/statuscol.nvim",
+    config = function()
+        require("statuscol").setup {
+            foldfunc = "builtin",
+            setopt = true,
+        }
+    end,
+}
+
+local ufo = {
+    'kevinhwang91/nvim-ufo',
+    requires = { 'kevinhwang91/promise-async', statuscol[1] },
+    config = function()
+        vim.o.foldcolumn = '1'
+        vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+        vim.o.foldlevelstart = 99
+        vim.o.foldenable = true
+        vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+        vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+        vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+
+        require('ufo').setup({
+            provider_selector = function(bufnr, filetype, buftype)
+                return { 'treesitter', 'indent' }
+            end
+        })
+    end,
+}
+
+return { ufo, statuscol }
