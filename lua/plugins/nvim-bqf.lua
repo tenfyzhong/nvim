b50cee000b9d6d7ee7ced887fdcc6847f5e46fb7
tenfy <tenfy@tenfy.cn>
2023-02-05 22:34:21 +0800

    bqf plugin

---
+++ lua/plugins/nvim-bqf.lua
@@ -0,0 +1,13 @@
+--[[
+- @file nvim-bqf.lua
+- @brief  
+- @author tenfyzhong
+- @email tenfy@tenfy.cn
+- @created 2023-02-05 22:26:50
+--]]
+local bqf = {
+    'kevinhwang91/nvim-bqf',
+    ft = 'qf'
+}
+
+return { bqf }
