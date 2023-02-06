dba304b0ef6022e047b49e5d10baaa9256f0e3c3
tenfy <tenfy@tenfy.cn>
2023-02-06 23:06:31 +0800

    lqa plugin

---
+++ lua/plugins/lqa.lua
@@ -0,0 +1,24 @@
+--[[
+- @file lqa.lua
+- @brief  
+- @author tenfyzhong
+- @email tenfy@tenfy.cn
+- @created 2023-02-05 22:38:01
+--]]
+local lqa = {
+    'tenfyzhong/lqa.nvim',
+    config = function()
+        require('lqa').setup {
+            keymap = {
+                previous = '<leader>qk',
+                next = '<leader>qj',
+                close = '<leader>qc',
+                open = '<leader>qo',
+                quickfix_open = '<leader>qq',
+                loclist_open = '<leader>ql',
+            }
+        }
+    end,
+}
+
+return { lqa }
