e3f6561f05096f581905622ca69003510c452203
tenfy <tenfy@tenfy.cn>
2023-02-08 00:46:08 +0800

    use hop

---
+++ lua/plugins/hop.lua
@@ -0,0 +1,67 @@
+--[[
+- @file hop.lua
+- @brief  
+- @author tenfyzhong
+- @email tenfy@tenfy.cn
+- @created 2023-02-07 23:59:35
+--]]
+
+local hop = {
+    'phaazon/hop.nvim',
+    branch = 'v2', -- optional but strongly recommended
+    config = function()
+        -- you can configure Hop the way you like here; see :h hop-config
+        local hop = require('hop')
+        hop.setup {
+            keys = 'asdfghjklqwertyuiopzxcvbnm'
+        }
+
+        local directions = require('hop.hint').HintDirection
+        local position = require('hop.hint').HintPosition
+        vim.keymap.set('n', '<tab>f', function()
+            hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = false })
+        end, { remap = true })
+        vim.keymap.set('n', '<tab>F', function()
+            hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = false })
+        end, { remap = true })
+        vim.keymap.set('n', '<tab>t', function()
+            hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = false, hint_offset = -1 })
+        end, { remap = true })
+        vim.keymap.set('n', '<tab>T', function()
+            hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = false, hint_offset = 1 })
+        end, { remap = true })
+
+        vim.keymap.set('n', '<tab>w', function()
+            hop.hint_words({ direction = directions.AFTER_CURSOR, current_line_only = false })
+        end, { remap = true })
+        vim.keymap.set('n', '<tab>W', function()
+            hop.hint_words({ direction = directions.AFTER_CURSOR, current_line_only = false, multi_windows = true })
+        end, { remap = true })
+        vim.keymap.set('n', '<tab>b', function()
+            hop.hint_words({ direction = directions.BEFORE_CURSOR, current_line_only = false })
+        end, { remap = true })
+        vim.keymap.set('n', '<tab>e', function()
+            hop.hint_words({ direction = directions.AFTER_CURSOR, current_line_only = false,
+                hint_position = position.END })
+        end, { remap = true })
+        vim.keymap.set('n', '<tab>ge', function()
+            hop.hint_words({ direction = directions.BEFORE_CURSOR, current_line_only = false,
+                hint_position = position.END })
+        end, { remap = true })
+
+        vim.keymap.set('n', '<tab>j', function()
+            hop.hint_lines({ direction = directions.AFTER_CURSOR, current_line_only = false })
+        end, { remap = true })
+        vim.keymap.set('n', '<tab>k', function()
+            hop.hint_lines({ direction = directions.BEFORE_CURSOR, current_line_only = false })
+        end, { remap = true })
+        vim.keymap.set('n', '<tab>J', function()
+            hop.hint_lines({ direction = directions.AFTER_CURSOR, current_line_only = false, multi_windows = true })
+        end, { remap = true })
+        vim.keymap.set('n', '<tab>K', function()
+            hop.hint_lines({ direction = directions.BEFORE_CURSOR, current_line_only = false, multi_windows = true })
+        end, { remap = true })
+    end
+}
+
+return { hop }
