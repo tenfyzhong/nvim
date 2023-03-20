ed836fa00f28064238520a9c0570263b6e3d3580
tenfy <tenfy@tenfy.cn>
2023-03-20 10:13:24 +0800

    desc yanky mapping

--- lua/plugins/yanky.lua
+++ lua/plugins/yanky.lua
@@ -10,12 +10,18 @@ local yanky = {
     'gbprod/yanky.nvim',
     config = function()
         require("yanky").setup {}
-        vim.keymap.set({ "n", "x" }, "p", "<Plug>(YankyPutAfter)")
-        vim.keymap.set({ "n", "x" }, "P", "<Plug>(YankyPutBefore)")
-        vim.keymap.set({ "n", "x" }, "gp", "<Plug>(YankyGPutAfter)")
-        vim.keymap.set({ "n", "x" }, "gP", "<Plug>(YankyGPutBefore)")
-        vim.keymap.set("n", "'yn", "<Plug>(YankyCycleForward)")
-        vim.keymap.set("n", "'yp", "<Plug>(YankyCycleBackward)")
+        vim.keymap.set({ "n", "x" }, "p", "<Plug>(YankyPutAfter)",
+            { silent = true, remap = true, desc = 'yanky: yank put after' })
+        vim.keymap.set({ "n", "x" }, "P", "<Plug>(YankyPutBefore)",
+            { silent = true, remap = true, desc = 'yanky: yank put before' })
+        vim.keymap.set({ "n", "x" }, "gp", "<Plug>(YankyGPutAfter)",
+            { silent = true, remap = true, desc = 'yanky: yank gput after' })
+        vim.keymap.set({ "n", "x" }, "gP", "<Plug>(YankyGPutBefore)",
+            { silent = true, remap = true, desc = 'yanky: yank gput after' })
+        vim.keymap.set("n", "'yn", "<Plug>(YankyCycleForward)",
+            { silent = true, remap = true, desc = 'yanky: cycle forward' })
+        vim.keymap.set("n", "'yp", "<Plug>(YankyCycleBackward)",
+            { silent = true, remap = true, desc = 'yanky: cycle backward' })
     end,
     event = 'VeryLazy',
 }
