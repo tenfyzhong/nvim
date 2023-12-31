6a6d6a8fa5187206ab5695933b99a77d1052b011
tenfy <tenfy@tenfy.cn>
2023-12-31 15:18:00 +0800

    plugin notice

---
+++ lua/plugins/notice.lua
@@ -0,0 +1,35 @@
+local notice = {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+        -- add any options here
+        cmdline = {
+            view = 'cmdline',
+            format = {
+                -- conceal: (default=true) This will hide the text in the cmdline that matches the pattern.
+                -- view: (default is cmdline view)
+                -- opts: any options passed to the view
+                -- icon_hl_group: optional hl_group for the icon
+                -- title: set to anything or empty string to hide
+                cmdline = { pattern = "^:", icon = "", lang = "vim" },
+                search_down = { kind = "search", pattern = "^/", icon = "", lang = "regex" },
+                search_up = { kind = "search", pattern = "^%?", icon = "", lang = "regex" },
+                filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
+                lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = "", lang = "lua" },
+                help = { pattern = "^:%s*he?l?p?%s+", icon = "" },
+                input = {}, -- Used by input()
+                -- lua = false, -- to disable a format, set to `false`
+            },
+        },
+    },
+    dependencies = {
+        -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+        "MunifTanjim/nui.nvim",
+        -- OPTIONAL:
+        --   `nvim-notify` is only needed, if you want to use the notification view.
+        --   If not available, we use `mini` as the fallback
+        "rcarriga/nvim-notify",
+    }
+}
+
+return { notice }
