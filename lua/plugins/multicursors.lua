deece2721a472f76e07a464169fd9bd19b9e282d
tenfy <tenfy@tenfy.cn>
2024-01-05 23:37:23 +0800

    use multicursors instead vim-visual-multi

---
+++ lua/plugins/multicursors.lua
@@ -0,0 +1,20 @@
+local multi = -- lazy.nvim:
+{
+    "smoka7/multicursors.nvim",
+    event = "VeryLazy",
+    dependencies = {
+        'smoka7/hydra.nvim',
+    },
+    opts = {},
+    cmd = { 'MCstart', 'MCvisual', 'MCclear', 'MCpattern', 'MCvisualPattern', 'MCunderCursor' },
+    keys = {
+        {
+            mode = { 'v', 'n' },
+            '<c-n>',
+            '<cmd>MCstart<cr>',
+            desc = 'Create a selection for selected text or word under the cursor',
+        },
+    },
+}
+
+return { multi }
