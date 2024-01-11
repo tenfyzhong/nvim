b2c5c6fa0c4671aa0d6ae947e66f3c1d93c6ea5e
tenfy <tenfy@tenfy.cn>
2024-01-11 20:50:39 +0800

    reset grepper mapping to gr

--- lua/plugins/vim-grepper.lua
+++ lua/plugins/vim-grepper.lua
@@ -1,6 +1,6 @@
 --[[
 - @file vim-grepper.lua
-- @brief  
+- @brief
 - @author tenfyzhong
 - @email tenfy@tenfy.cn
 - @created 2023-01-27 23:03:42
@@ -11,13 +11,15 @@ local grepper = {
     config = function()
         vim.g.grepper = {
             highlight = 1,
-            tools = { 'ag' },
+            tools = { 'rg', 'ag' },
+            dir = 'repo,cwd',
         }
 
         vim.api.nvim_create_user_command('Todo',
-            "Grepper -noprompt -tool ag -grepprg ag --vimgrep '(\bTODO\b|\bBUG\b|\bERROR\b)'", {})
-        vim.keymap.set({ 'n', 'x' }, 'gp', '<plug>(GrepperOperator)', { remap = true, desc = 'grpper: grep' })
-        vim.keymap.set({ 'n' }, '<leader>*', ':Grepper -tool ag -cword -noprompt<cr>',
+            "Grepper -noprompt -tool rg -grepprg rg --vimgrep '(// todo\\b|// bug\\b|// error\\b)'",
+            { desc = 'grepper: Find todo bug error' })
+        vim.keymap.set({ 'n', 'x' }, 'gr', '<plug>(GrepperOperator)', { remap = true, desc = 'grepper: grep' })
+        vim.keymap.set({ 'n' }, '<leader>*', ':Grepper -tool rg -cword -noprompt<cr>',
             { remap = false, desc = 'grepper: grep cword' })
     end,
     event = 'VeryLazy',
