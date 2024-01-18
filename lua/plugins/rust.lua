75c1781ca43ed760a447e23c9d62f410ef7b8932
tenfyzhong <tenfy@tenfy.cn>
2024-01-18 23:55:44 +0800

    rust plugin

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -1,31 +1,16 @@
 --[[
 - @file rust.lua
-- @brief  
+- @brief
 - @author tenfyzhong
 - @email tenfy@tenfy.cn
 - @created 2023-01-27 18:59:21
 --]]
 
-local rust = {
-    'rust-lang/rust.vim',
-    ft = 'rust',
-    config = function()
-        vim.g.rustfmt_autosave = 0
-        vim.g.rustfmt_fail_silently = 1
-
-        local group = vim.api.nvim_create_augroup('rust_init', {})
-        vim.api.nvim_create_autocmd('FileType', {
-            group = group,
-            pattern = 'rust',
-            callback = function()
-                vim.keymap.set('n', '<leader>rr',
-                    function()
-                        vim.cmd('RustRun')
-                    end, { buffer = true, silent = true, remap = false, desc = 'rust: RustRun' })
-            end,
-        })
-    end,
-    event = 'VeryLazy',
+local rustaceanvim = {
+    'mrcjkb/rustaceanvim',
+    version = '^3', -- Recommended
+    ft = { 'rust' },
 }
 
-return { rust }
+
+return { rustaceanvim }
