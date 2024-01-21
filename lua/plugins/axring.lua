19dc0c8ee3e223d9024bdec46737aa1ede2b2128
tenfyzhong <tenfy@tenfy.cn>
2024-01-21 17:39:05 +0800

    axring rust config

--- lua/plugins/axring.lua
+++ lua/plugins/axring.lua
@@ -140,6 +140,12 @@ local axring = {
                 "image"
             },
         }
+
+        vim.g.axring_rings_rust = {
+            { "i8",  "i16", "i32", "i64", "isize" },
+            { "u8",  "u16", "u32", "u64", "usize" },
+            { "f32", "f64" },
+        }
     end,
     event = 'VeryLazy',
 }
