805e42142054850d186c6d20e166a6cb10576733
tenfyzhong <tenfy@tenfy.cn>
2024-02-06 01:22:54 +0800

    remove codeium

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -98,7 +98,7 @@ local function cmp_config()
                 mode = 'symbol',       -- show only symbol annotations
                 maxwidth = 50,         -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
                 ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-                symbol_map = { Codeium = "", },
+                -- symbol_map = { Codeium = "", },
 
                 -- The function below will be called before any actual modifications from lspkind
                 -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
