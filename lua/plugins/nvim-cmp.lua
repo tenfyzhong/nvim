41b9c26c89990665b13c408f8cab0af2abe5eab6
tenfyzhong <tenfy@tenfy.cn>
2024-02-10 23:54:41 +0800

    cmp-git config

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -223,7 +223,116 @@ local cmp_nvim_lsp_signature_help = { 'hrsh7th/cmp-nvim-lsp-signature-help' }
 local neodev = { 'folke/neodev.nvim', ft = 'lua' }
 -- local plenary = { 'nvim-lua/plenary.nvim' }
 
-local cmp_git = { 'petertriho/cmp-git', ft = { 'git', 'gitcommit' } }
+local cmp_git = {
+    'petertriho/cmp-git',
+    ft = { 'gitcommit', 'octo' },
+    config = function()
+        local format = require("cmp_git.format")
+        local sort = require("cmp_git.sort")
+
+        require("cmp_git").setup({
+            -- defaults
+            filetypes = { "gitcommit", "octo" },
+            remotes = { "origin", "upstream" }, -- in order of most to least prioritized
+            enableRemoteUrlRewrites = true,     -- enable git url rewrites, see https://git-scm.com/docs/git-config#Documentation/git-config.txt-urlltbasegtinsteadOf
+            git = {
+                commits = {
+                    limit = 100,
+                    sort_by = sort.git.commits,
+                    format = format.git.commits,
+                },
+            },
+            github = {
+                hosts = { 'github.com' }, -- list of private instances of github
+                issues = {
+                    fields = { "title", "number", "body", "updatedAt", "state" },
+                    filter = "all", -- assigned, created, mentioned, subscribed, all, repos
+                    limit = 100,
+                    state = "open", -- open, closed, all
+                    sort_by = sort.github.issues,
+                    format = format.github.issues,
+                },
+                mentions = {
+                    limit = 100,
+                    sort_by = sort.github.mentions,
+                    format = format.github.mentions,
+                },
+                pull_requests = {
+                    fields = { "title", "number", "body", "updatedAt", "state" },
+                    limit = 100,
+                    state = "open", -- open, closed, merged, all
+                    sort_by = sort.github.pull_requests,
+                    format = format.github.pull_requests,
+                },
+            },
+            gitlab = {
+                hosts = { 'gitlab.com' }, -- list of private instances of gitlab
+                issues = {
+                    limit = 100,
+                    state = "opened", -- opened, closed, all
+                    sort_by = sort.gitlab.issues,
+                    format = format.gitlab.issues,
+                },
+                mentions = {
+                    limit = 100,
+                    sort_by = sort.gitlab.mentions,
+                    format = format.gitlab.mentions,
+                },
+                merge_requests = {
+                    limit = 100,
+                    state = "opened", -- opened, closed, locked, merged
+                    sort_by = sort.gitlab.merge_requests,
+                    format = format.gitlab.merge_requests,
+                },
+            },
+            trigger_actions = {
+                {
+                    debug_name = "git_commits",
+                    trigger_character = ":",
+                    action = function(sources, trigger_char, callback, params, git_info)
+                        return sources.git:get_commits(callback, params, trigger_char)
+                    end,
+                },
+                {
+                    debug_name = "gitlab_issues",
+                    trigger_character = "#",
+                    action = function(sources, trigger_char, callback, params, git_info)
+                        return sources.gitlab:get_issues(callback, git_info, trigger_char)
+                    end,
+                },
+                {
+                    debug_name = "gitlab_mentions",
+                    trigger_character = "@",
+                    action = function(sources, trigger_char, callback, params, git_info)
+                        return sources.gitlab:get_mentions(callback, git_info, trigger_char)
+                    end,
+                },
+                {
+                    debug_name = "gitlab_mrs",
+                    trigger_character = "!",
+                    action = function(sources, trigger_char, callback, params, git_info)
+                        return sources.gitlab:get_merge_requests(callback, git_info, trigger_char)
+                    end,
+                },
+                {
+                    debug_name = "github_issues_and_pr",
+                    trigger_character = "#",
+                    action = function(sources, trigger_char, callback, params, git_info)
+                        return sources.github:get_issues_and_prs(callback, git_info, trigger_char)
+                    end,
+                },
+                {
+                    debug_name = "github_mentions",
+                    trigger_character = "@",
+                    action = function(sources, trigger_char, callback, params, git_info)
+                        return sources.github:get_mentions(callback, git_info, trigger_char)
+                    end,
+                },
+            },
+        }
+        )
+    end,
+}
 
 local crates = {
     "Saecki/crates.nvim",
