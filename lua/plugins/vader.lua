e0c5af89077dec531b743ec6a8a5725f9632e68d
tenfyzhong <tenfy@tenfy.cn>
2024-03-31 11:17:49 +0800

    plugin vader

---
+++ lua/plugins/vader.lua
@@ -0,0 +1,15 @@
+--[[
+- @file vader.lua
+- @brief
+- @author tenfyzhong
+- @email tenfy@tenfy.cn
+- @created 2024-03-22 11:02:57
+--]]
+
+local vader = {
+    'junegunn/vader.vim',
+    event = 'VeryLazy',
+    enable = true,
+}
+
+return { vader }
