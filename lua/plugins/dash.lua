30d4c9e91aea41c0d8ec5e7c686ef616b7a96e58
tenfyzhong <tenfy@tenfy.cn>
2024-04-10 22:46:58 +0800

    feat(dash): add plugin dash.nvim

--- lua/plugins/dash.lua
+++ lua/plugins/dash.lua
@@ -6,13 +6,45 @@
 - @created 2023-01-27 20:54:20
 --]]
 local dash = {
-    'rizzatti/dash.vim',
+    'tenfyzhong/dash.nvim',
     config = function()
+        require('dash').setup({
+            -- configure path to Dash.app if installed somewhere other than /Applications/Dash.app
+            dash_app_path = '/Applications/Dash.app',
+            -- search engine to fall back to when Dash has no results, must be one of: 'ddg', 'duckduckgo', 'startpage', 'google'
+            search_engine = 'ddg',
+            -- debounce while typing, in milliseconds
+            debounce = 1,
+            -- map filetype strings to the keywords you've configured for docsets in Dash
+            -- setting to false will disable filtering by filetype for that filetype
+            -- filetypes not included in this table will not filter the query by filetype
+            -- check src/lua_bindings/dash_config_binding.rs to see all defaults
+            -- the values you pass for file_type_keywords are merged with the defaults
+            -- to disable filtering for all filetypes,
+            -- set file_type_keywords = false
+            file_type_keywords = {
+                dashboard = false,
+                NvimTree = false,
+                TelescopePrompt = false,
+                terminal = false,
+                packer = false,
+                fzf = false,
+                -- a table of strings will search on multiple keywords
+                javascript = { 'javascript', 'nodejs' },
+                typescript = { 'typescript', 'javascript', 'nodejs' },
+                typescriptreact = { 'typescript', 'javascript', 'react' },
+                javascriptreact = { 'javascript', 'react' },
+                -- you can also do a string, for example,
+                -- sh = 'bash'
+            },
+        })
     end,
+    dependencies = { 'nvim-telescope/telescope.nvim' },
+    build = 'make install',
+    cmd = { 'Dash', 'DashWord', 'DashDirect', 'DashDirectWord' },
     keys = {
-        { '<leader>ds', '<Plug>DashSearch', silent = true, remap = true, desc = 'dash: search cword' },
+        { '<leader>ds', ':DashDirectWord<CR>', silent = true, remap = false, desc = 'dash: search cword' },
     },
-    cmd = { 'Dash', 'DashKeywords' },
 }
 
 return { dash }
