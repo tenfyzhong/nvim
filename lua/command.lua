b1f9226f2b3c76830c2cc5a38ac2228bbe491e9a
tenfyzhong <tenfy@tenfy.cn>
2024-04-29 22:02:17 +0800

    feat(command): Add XXD command to edit binary file

---
+++ lua/command.lua
@@ -0,0 +1,13 @@
+--[[
+- @file command.lua
+- @brief
+- @author tenfyzhong
+- @email tenfy@tenfy.cn
+- @created 2024-04-29 21:00:34
+--]]
+
+
+vim.api.nvim_create_user_command('XXD', function()
+        require('feature').XXD()
+    end,
+    { desc = "Use xxd to edit the current buffer" })
--- lua/feature.lua
+++ lua/feature.lua
@@ -12,3 +12,20 @@ function PollNumber()
         vim.o.relativenumber = true
     end
 end
+
+function XXD()
+    if vim.b.is_xxd == nil then
+        vim.b.is_xxd = false
+    end
+    local mod = vim.o.mod
+    if vim.b.is_xxd then
+        vim.o.binary = false
+        vim.cmd('silent %!xxd -r')
+        vim.b.is_xxd = false
+    else
+        vim.binary = true
+        vim.cmd('silent %!xxd')
+        vim.b.is_xxd = true
+    end
+    vim.o.mod = mod
+end
