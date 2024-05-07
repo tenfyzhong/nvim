1401107e37baac14fb698935232404bafe1801e2
tenfyzhong <tenfy@tenfy.cn>
2024-05-07 23:53:50 +0800

    feat(zoxide): init

---
+++ lua/plugins/zoxide.lua
@@ -0,0 +1,14 @@
+--[[
+- @file zoxide.lua
+- @brief
+- @author tenfyzhong
+- @email tenfy@tenfy.cn
+- @created 2024-05-07 22:57:21
+--]]
+
+local zoxide = {
+    'nanotee/zoxide.vim',
+    cmd = { 'Z', 'Lz', 'Tz', 'Zi', 'Lzi', 'Tzi' },
+}
+
+return { zoxide }
