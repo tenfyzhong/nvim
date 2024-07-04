6d4d56bbfcbcfa0cfcb60eb7e04bd1ec7497f0a6
tenfyzhong <tenfy@tenfy.cn>
2024-07-04 20:50:04 +0800

    feat(cmp): insert git-commit-msg when select a git commit
    Create format object and then rewrite the `insertText` member to insert the
    commit message

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -246,30 +246,13 @@ local cmp_git = {
         local sort = require("cmp_git.sort")
 
         local commits = {
-            label = function(trigger_char, commit)
-                return string.format("%s: %s", commit.sha, commit.title)
-            end,
-            filterText = function(trigger_char, commit)
-                -- If the trigger char is not part of the label, no items will show up
-                return string.format("%s %s %s", trigger_char, commit.sha, commit.title)
-            end,
+            label = format.git.commits.label,
+            filterText = format.git.commits.filterText,
             insertText = function(trigger_char, commit)
                 return (commit.description == nil or commit.description == '') and commit.title or
-                string.format("%s\n\n%s", commit.title, commit.description)
+                    string.format("%s\n\n%s", commit.title, commit.description)
             end,
-            documentation = function(trigger_char, commit)
-                return {
-                    kind = "markdown",
-                    value = string.format(
-                        "# %s\n\n%s\n\nCommited by %s (%s) on %s",
-                        commit.title,
-                        commit.description,
-                        commit.author_name,
-                        commit.author_mail,
-                        os.date("%c", commit.commit_timestamp)
-                    ),
-                }
-            end,
+            documentation = format.git.commits.documentation,
         }
 
         require("cmp_git").setup({
