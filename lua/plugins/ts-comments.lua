9b65bccc0ac5b00f4a6b2984d4a4150e5366a728
tenfyzhong <tenfy@tenfy.cn>
2024-07-08 13:48:41 +0800

    feat(ts-comments): Use ts-comments instead Comment.nvim
    neovim 0.10.0 has supported commenting, so the Comment.nvim is not need anymore.
    But the ts-comments enhance the native commenting, so install it instead.

---
+++ lua/plugins/ts-comments.lua
@@ -0,0 +1,13 @@
+--[[
+- @file ts-comments.lua
+- @brief
+- @author tenfyzhong
+- @email tenfy@tenfy.cn
+- @created 2024-07-08 13:45:54
+--]]
+local comments = {
+  "folke/ts-comments.nvim",
+  event = "VeryLazy",
+}
+
+return {comments}
