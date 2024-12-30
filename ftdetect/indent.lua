84b6c74ba9347357b475f647ef9a1f3adc6c6868
tenfyzhong <tenfy@tenfy.cn>
2024-12-31 00:24:42 +0800

    feat(indent): disable cindent for sed

--- ftdetect/indent.lua
+++ ftdetect/indent.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_create_autocmd({ 'FileType' }, {
-    pattern = { 'gitcommit' },
+    pattern = { 'gitcommit', 'sed' },
     callback = function()
         vim.bo.cindent = false
     end,
