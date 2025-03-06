5f508aafd0dd9f5ecd27fe1b56567fd898abadd0
tenfyzhong <tenfy@tenfy.cn>
2025-03-06 14:17:52 +0800

    feat(codecompanion): markdown highlight

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -48,6 +48,7 @@ local function treesister_config()
         highlight = {
             -- `false` will disable the whole extension
             enable = true,
+            -- enable = { 'markdown', 'codecompanion' },
 
             -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
             -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
@@ -56,6 +57,11 @@ local function treesister_config()
             -- disable = { "c", "rust" },
             -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
             disable = function(lang, buf)
+                local ft = vim.bo[buf].filetype
+                if ft ~= "markdown" and ft ~= "codecompanion" then
+                    return true
+                end
+
                 local max_filesize = 100 * 1024 -- 100 KB
                 local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
                 if ok and stats and stats.size > max_filesize then
