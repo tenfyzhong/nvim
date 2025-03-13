e59b7b96d5417f6610f77507916150c537ee4896
tenfyzhong <tenfy@tenfy.cn>
2025-03-13 11:39:29 +0800

    feat(keymap): remove diff maps

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -13,24 +13,6 @@ local function write_redraw()
     print(':wa')
 end
 
-local function diffget()
-    if not vim.o.diff then
-        vim.cmd('echohl ErrorMsg | echom "Current buffer is not in diff mode" | echohl None')
-        return
-    end
-    vim.cmd('diffget')
-    vim.cmd('diffupdate')
-end
-
-local function diffput()
-    if not vim.o.diff then
-        vim.cmd('echohl ErrorMsg | echom "Current buffer is not in diff mode" | echohl None')
-        return
-    end
-    vim.cmd('diffput')
-    vim.cmd('diffupdate')
-end
-
 local function j()
     local count = vim.v.count
     if count == 0 then
@@ -59,8 +41,6 @@ vim.keymap.set({ 'i', 'c' }, '<c-j>', '<Down>', { silent = true, remap = false }
 vim.keymap.set({ 'n', 'x' }, ';', ':', { silent = false, remap = false })
 vim.keymap.set({ 'n' }, ',', ';', { silent = true, remap = false })
 vim.keymap.set({ 'n' }, '<leader>w', write_redraw, { silent = true, remap = false })
-vim.keymap.set({ 'n' }, 'do', diffget, { silent = true, remap = false })
-vim.keymap.set({ 'n' }, 'dp', diffput, { silent = true, remap = false })
 vim.keymap.set({ 'n' }, '<leader>nn', function() require('feature').poll_number() end, { silent = true, remap = false })
 vim.keymap.set({ 'v' }, '<', '<gv', { remap = false })
 vim.keymap.set({ 'v' }, '>', '>gv', { remap = false })
