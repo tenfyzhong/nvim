c03f0be1de46574283851ffd823b4f2d41574f45
tenfyzhong <tenfy@tenfy.cn>
2025-07-07 23:59:44 +0800

    refactor(go.nvim): Remove custom goimports logic and autocommands
    - Formatting is now handled by `gopls` via `nvim-lspconfig`.
    - Removes redundant custom `goimports` implementation.
    - Simplifies plugin configuration.
    
    Signed-off-by: tenfyzhong <tenfy@tenfy.cn>

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -6,104 +6,6 @@
 - @created 2023-02-01 00:12:36
 --]]
 
-local run = function(fmtargs, bufnr, cmd)
-    local utils = require('go.utils')
-    local api = vim.api
-    local vfn = vim.fn
-
-    bufnr = bufnr or vim.api.nvim_get_current_buf()
-    if vim.o.mod == true then
-        vim.cmd('noautocmd silent write')
-    end
-
-    local oldtick = vfn.getbufvar(bufnr, 'changedtick')
-    -- local oldwinnr = vfn.winnr()
-    -- local winview = vfn.winsaveview()
-
-    local args = vim.deepcopy(fmtargs)
-    local bufname = api.nvim_buf_get_name(bufnr)
-    table.insert(args, bufname)
-
-    local old_lines = api.nvim_buf_get_lines(0, 0, -1, true)
-    table.insert(args, 1, cmd)
-
-    local command = table.concat(args, ' ')
-
-    vfn.jobstart(command, {
-        on_stdout = function(_, data, _)
-            -- if the changedtick incremented, the file was edited after the
-            -- format action. so the format result is outdated
-            local newtick = vfn.getbufvar(bufnr, 'changedtick')
-            if newtick > oldtick then
-                return
-            end
-
-            data = utils.handle_job_data(data)
-            if not data then
-                return
-            end
-
-            if not utils.check_same(old_lines, data) then
-                vim.notify('updating codes', vim.log.levels.DEBUG)
-
-                require('feature').format(function()
-                    api.nvim_buf_set_lines(bufnr, 0, -1, false, data)
-                end)
-
-                vim.notify('autoformatted.', vim.log.levels.INFO)
-            end
-        end,
-        on_stderr = function(_, data, _)
-            data = utils.handle_job_data(data)
-            if data then
-                vim.notify(vim.inspect(data) .. ' from stderr', vim.log.levels.DEBUG)
-            end
-        end,
-        on_exit = function(_, data, _) -- id, data, event
-            if data ~= 0 then
-                return vim.notify('goimports failed ' .. tostring(data), vim.log.levels.ERROR)
-            end
-            old_lines = nil
-            vim.defer_fn(function()
-                if vfn.getbufinfo(bufnr)[1].changed == 1 then
-                    vim.cmd('noautocmd silent write')
-                end
-            end, 200)
-        end,
-        stdout_buffered = true,
-        stderr_buffered = true,
-    })
-end
-
-local table_contains = function(tbl, x)
-    local found = false
-    for _, v in pairs(tbl) do
-        if v == x then
-            found = true
-        end
-    end
-    return found
-end
-
-local imports = function(...)
-    if vim.o.diff then
-        return
-    end
-
-    local goimport = 'goimports'
-    local args = { ... }
-
-    local l = os.getenv('GOIMPORTS_LOCAL')
-    if l ~= nil and not table_contains(args, '-local') and l ~= '' then
-        table.insert(args, '-local')
-        table.insert(args, l)
-    end
-
-    local buf = vim.api.nvim_get_current_buf()
-    require('go.install').install(goimport)
-    return run(args, buf, goimport)
-end
-
 local go = {
     'ray-x/go.nvim',
     config = function()
@@ -114,24 +16,6 @@ local go = {
             -- goimport = 'goimports',
             -- fillstruct = 'fillstruct',
         }
-
-        -- vim.api.nvim_create_user_command('GoImports', function(opts)
-        --     imports(unpack(opts.fargs))
-        -- end, {
-        --     desc = 'go.vim: goimports, use env<$GOIMPORTS_LOCAL> to set the -local option',
-        --     nargs = '*',
-        -- })
-
-        -- Run gofmt on save
-        -- local format_sync_grp = vim.api.nvim_create_augroup("go_format", {})
-        -- vim.api.nvim_create_autocmd("BufWritePre", {
-        --     pattern = "*.go",
-        --     callback = function()
-        --         -- require('go.format').gofmt()
-        --         imports('-format-only')
-        --     end,
-        --     group = format_sync_grp,
-        -- })
 
         local go_nvim_local_group = vim.api.nvim_create_augroup('go_nvim_local', {})
         vim.api.nvim_create_autocmd('FileType', {
@@ -175,9 +59,6 @@ local go = {
                     { buffer = true, remap = false, silent = true, desc = 'go.nvim: GoAltS' })
                 vim.keymap.set('n', '<leader>av', ':GoAltS<cr>',
                     { buffer = true, remap = false, silent = true, desc = 'go.nvim: GoAltV' })
-                vim.keymap.set('n', '<leader>af',
-                    ':silent GoImports<cr>',
-                    { buffer = true, remap = false, silent = true, desc = 'go.nvim: GoImports' })
             end,
         })
     end,
@@ -186,7 +67,6 @@ local go = {
     end,
     ft = 'go',
     dependencies = { 'neovim/nvim-lspconfig', 'nvim-treesitter/nvim-treesitter', 'mfussenegger/nvim-dap' },
-    goimports = imports,
 }
 
 return { go }
