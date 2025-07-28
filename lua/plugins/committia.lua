4f4b27dd002fdf1b271c43298ab650f4dc4bba00
tenfyzhong <tenfy@tenfy.cn>
2025-07-28 19:11:31 +0800

    feat(committia): improve status window usability
    - Add a Conventional Commits header to the commit message buffer to serve as a quick reference guide.
    - Dynamically resize the status window based on the number of changed files, preventing it from becoming too large.
    
    Signed-off-by: tenfyzhong <tenfy@tenfy.cn>

--- lua/plugins/committia.lua
+++ lua/plugins/committia.lua
@@ -7,9 +7,10 @@
 --]]
 
 local committia = {
-    'rhysd/committia.vim',
+    "rhysd/committia.vim",
     config = function()
         vim.cmd([[
+let g:committia_status_window_min_height = 20
 let g:committia_hooks = {}
 function! g:committia_hooks.edit_open(info)
     setlocal nonu
@@ -33,11 +34,29 @@ function! g:committia_hooks.status_open(info)
         autocmd InsertLeave * set nonumber norelativenumber
     augroup END
 
+    let l:old_a = getreg('a')
+    let comment =<< END
+################## Conventional Commits Standard ##################
+# type: fix/feat/build/chroe/ci/docs/style/refactor/perf/test/BREAK
+###################################################################
+END
+    call setreg('a', comment)
+    set modifiable
+    normal! gg"aP
+    set nomodifiable
+    normal! gg
+    call setreg('a', l:old_a)
+
+    let lines = line('$')
+    let max_height = g:committia_status_window_min_height != 0 ? g:committia_status_window_min_height : 20
+    let height = lines > max_height ? max_height : lines
+    execute 'resize ' . height
+
 endfunction
 
         ]])
     end,
-    ft = 'gitcommit',
+    ft = "gitcommit",
 }
 
 return { committia }
