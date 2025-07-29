d7d6f93fc7c8e9eb2f30c86ee270f1b57f69e9c8
tenfyzhong <tenfy@tenfy.cn>
2025-07-29 20:23:25 +0800

    feat(todo-comments): Add FORBIDDEN-COMMITTING keyword
    - Introduce `FORBIDDEN-COMMITTING` as a new keyword to highlight code that must not be committed.
    - Update `jump_next` and `jump_prev` keymaps to include the new keyword for navigation.
    - Standardize string literals to use double quotes for consistency.
    
    Signed-off-by: tenfyzhong <tenfy@tenfy.cn>

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,10 +1,10 @@
 local todo_comments = {
-    'folke/todo-comments.nvim',
+    "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
-    event = 'VeryLazy',
+    event = "VeryLazy",
     config = function()
-        require('todo-comments').setup({
-            signs = true,      -- show icons in the signs column
+        require("todo-comments").setup({
+            signs = true, -- show icons in the signs column
             sign_priority = 8, -- sign priority
             -- keywords recognized as todo comments
             keywords = {
@@ -20,11 +20,12 @@ local todo_comments = {
                 PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
                 NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
                 TEST = { icon = "󰙨 ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
-                ERROR = { icon = " ", color = "error" }
+                ERROR = { icon = " ", color = "error" },
+                ["FORBIDDEN-COMMITTING"] = { icon = " ", color = "error" },
             },
             gui_style = {
-                fg = "NONE",       -- The gui style to use for the fg highlight group.
-                bg = "BOLD",       -- The gui style to use for the bg highlight group.
+                fg = "NONE", -- The gui style to use for the fg highlight group.
+                bg = "BOLD", -- The gui style to use for the bg highlight group.
             },
             merge_keywords = true, -- when true, custom keywords will be merged with the defaults
             -- highlighting of the line containing the todo comment
@@ -32,16 +33,16 @@ local todo_comments = {
             -- * keyword: highlights of the keyword
             -- * after: highlights after the keyword (todo text)
             highlight = {
-                multiline = true,                                                         -- enable multine todo comments
-                multiline_pattern = "^.",                                                 -- lua pattern to match the next multiline from the start of the matched keyword
-                multiline_context = 10,                                                   -- extra lines that will be re-evaluated when changing a line
-                before = "",                                                              -- "fg" or "bg" or empty
-                keyword = "bg",                                                           -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
-                after = "fg",                                                             -- "fg" or "bg" or empty
+                multiline = true,                                             -- enable multine todo comments
+                multiline_pattern = "^.",                                     -- lua pattern to match the next multiline from the start of the matched keyword
+                multiline_context = 10,                                       -- extra lines that will be re-evaluated when changing a line
+                before = "",                                                  -- "fg" or "bg" or empty
+                keyword = "bg",                                               -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
+                after = "fg",                                                 -- "fg" or "bg" or empty
                 pattern = [[\s+<(KEYWORDS)> \w+ \d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.*$]], -- pattern or table of patterns, used for highlighting (vim regex)
-                comments_only = true,                                                     -- uses treesitter to match keywords in comments only
-                max_line_len = 400,                                                       -- ignore lines longer than this
-                exclude = {},                                                             -- list of file types to exclude highlighting
+                comments_only = true,                                         -- uses treesitter to match keywords in comments only
+                max_line_len = 400,                                           -- ignore lines longer than this
+                exclude = {},                                                 -- list of file types to exclude highlighting
             },
             -- list of named colors where we try to extract the guifg from the
             -- list of highlight groups or use the hex color if hl not found as a fallback
@@ -51,7 +52,7 @@ local todo_comments = {
                 info = { "DiagnosticInfo", "#2563EB" },
                 hint = { "DiagnosticHint", "#10B981" },
                 default = { "Identifier", "#7C3AED" },
-                test = { "Identifier", "#FF00FF" }
+                test = { "Identifier", "#FF00FF" },
             },
             search = {
                 command = "rg",
@@ -71,11 +72,15 @@ local todo_comments = {
         })
 
         vim.keymap.set("n", "]t", function()
-            require("todo-comments").jump_next({ keywords = { "FIX", "TODO", "WARN", "PERF", "TEST", "ERROR" } })
+            require("todo-comments").jump_next({
+                keywords = { "FIX", "TODO", "WARN", "PERF", "TEST", "ERROR", "FORBIDDEN-COMMITTING" },
+            })
         end, { desc = "Next todo comment" })
 
         vim.keymap.set("n", "[t", function()
-            require("todo-comments").jump_prev({ keywords = { "FIX", "TODO", "WARN", "PERF", "TEST", "ERROR" } })
+            require("todo-comments").jump_prev({
+                keywords = { "FIX", "TODO", "WARN", "PERF", "TEST", "ERROR", "FORBIDDEN-COMMITTING" },
+            })
         end, { desc = "Previous todo comment" })
     end,
 }
