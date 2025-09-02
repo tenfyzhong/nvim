988f2b1e32b1b2530557d3df5da82826f753d700
tenfyzhong <tenfy@tenfy.cn>
2025-09-02 21:50:16 +0800

    feat(fzf): Switch to fzf-lua plugin
    - Replaced `junegunn/fzf.vim` with `ibhagwan/fzf-lua` for FZF functionality.
    - Updated keybindings to use `fzf-lua` commands.
    - Integrated `aerial.nvim` with `fzf-lua`'s picker by changing `find_tag`'s implementation.
    - Removed `tenfyzhong/z.nvim` as `fzf-lua` provides built-in zoxide support.
    - Added `nvim-tree/nvim-web-devicons` as a dependency for `fzf-lua` for icon support.
    
    Signed-off-by: tenfyzhong <tenfy@tenfy.cn>

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -6,117 +6,255 @@
 - @created 2023-01-27 20:06:26
 --]]
 
-
 local fzf = {
-    'junegunn/fzf',
-    build = ":call fzf#install()",
-    lazy = true,
+	"junegunn/fzf",
+	build = ":call fzf#install()",
+	lazy = true,
 }
 
 local function find_tag()
-    if vim.bo.filetype == 'thrift' then
-        vim.cmd('silent FZFBTags')
-    elseif vim.bo.filetype == 'NvimTree' or vim.bo.filetype == 'neo-tree' or vim.bo.filetype == 'aerial' then
-        vim.cmd('silent FZFBLines')
-    elseif vim.bo.filetype == 'fish' then
-        vim.cmd('silent FZFBTags')
-    else
-        vim.fn['aerial#fzf']()
-    end
+	if vim.bo.filetype == "thrift" then
+		vim.cmd("silent FZFBTags")
+	elseif vim.bo.filetype == "NvimTree" or vim.bo.filetype == "neo-tree" or vim.bo.filetype == "aerial" then
+		vim.cmd("silent FZFBLines")
+	elseif vim.bo.filetype == "fish" then
+		vim.cmd("silent FZFBTags")
+	else
+		require("aerial").fzf_lua_picker()
+	end
 end
 
+local fzf_lua = {
+	"ibhagwan/fzf-lua",
+	-- optional for icon support
+	dependencies = { "nvim-tree/nvim-web-devicons", fzf },
+	config = function()
+		require("fzf-lua").setup({
+			"hide",
+		})
+	end,
+	keys = {
+		{
+			"<leader>ff",
+			function()
+				require("fzf-lua").files()
+			end,
+			silent = true,
+			remap = false,
+			desc = "fzf-lua: files",
+		},
+		{
+			"<leader>fb",
+			function()
+				require("fzf-lua").buffers()
+			end,
+			silent = true,
+			remap = false,
+			desc = "fzf-lua: buffers",
+		},
+		{
+			"<leader>fg",
+			function()
+				require("fzf-lua").git_files()
+			end,
+			silent = true,
+			remap = false,
+			desc = "fzf-lua: git_files",
+		},
+		{
+			"<leader>fr",
+			function()
+				require("fzf-lua").grep_project()
+			end,
+			silent = true,
+			remap = false,
+			desc = "fzf-lua: grep_project",
+		},
+		{
+			"<leader>fh",
+			function()
+				require("fzf-lua").command_history()
+			end,
+			silent = true,
+			remap = false,
+			desc = "fzf-lua: command_history",
+		},
+		{
+			"<leader>fc",
+			function()
+				require("fzf-lua").commands()
+			end,
+			silent = true,
+			remap = false,
+			desc = "fzf-lua: commands",
+		},
+		{
+			"<leader>f/",
+			function()
+				require("fzf-lua").search_history()
+			end,
+			silent = true,
+			remap = false,
+			desc = "fzf-lua: search_history",
+		},
+		{
+			"<leader>fT",
+			function()
+				require("fzf-lua").tags()
+			end,
+			silent = true,
+			remap = false,
+			desc = "fzf-lua: tags",
+		},
+		{
+			"<leader>ft",
+			find_tag,
+			silent = true,
+			remap = false,
+			desc = "fzf-lua: buffer tags",
+		},
+		{
+			"<leader>fm",
+			function()
+				require("fzf-lua").marks()
+			end,
+			silent = true,
+			remap = false,
+			desc = "fzf-lua: marks",
+		},
+		{
+			"<leader>fz",
+			function()
+				require("fzf-lua").zoxide()
+			end,
+			silent = true,
+			remap = false,
+			desc = "fzf-lua: zoxide",
+		},
+		{
+			"<leader><leader>",
+			function()
+				require("fzf-lua").keymaps()
+			end,
+			silent = true,
+			remap = false,
+			desc = "fzf-lua: keymaps",
+		},
+	},
+	cmd = { "FzfLua" },
+}
+
 local fzf_vim = {
-    'junegunn/fzf.vim',
-    -- event = 'VeryLazy',
-    dependencies = { 'stevearc/aerial.nvim', fzf[1] },
-    init = function()
-        vim.g.fzf_command_prefix = 'FZF'
-        vim.g.fzf_history_dir = '~/.fzf-history'
-    end,
-    config = function() end,
-    cmd = { 'FZF', 'FZFFiles', 'FZFGFiles', 'FZFBuffers', 'FZFRg', 'FZFAg' },
-    keys = {
-        { '<leader>ff', ':FZFFiles<cr>',   silent = true, remap = false, desc = 'fzf: files' },
-        { '<leader>fg', ':FZFGFiles<cr>',  silent = true, remap = false, desc = 'fzf: git files' },
-        { '<leader>fb', ':FZFBuffers<cr>', silent = true, remap = false, desc = 'fzf: buffers' },
-        { '<leader>fr', ':FZFRg<cr>',      silent = true, remap = false, desc = 'fzf: rg' },
-        { '<leader>fa', ':FZFAg<cr>',      silent = true, remap = false, desc = 'fzf: ag' },
-        {
-            '<leader>fA',
-            function()
-                local cword = vim.fn.expand('<cword>')
-                vim.cmd(vim.g.fzf_command_prefix .. 'Ag ' .. cword .. '<cr>')
-            end,
-            silent = true,
-            remap = false,
-            desc = 'fzf: ag cword'
-        },
-        { '<leader>fh',       ':FZFHistory<cr>',    silent = true, remap = false,  desc = 'fzf: command history' },
-        { '<leader>fw',       ':FZFWindows<cr>',    silent = true, remap = false,  desc = 'fzf: windows' },
-        { '<leader>fc',       ':FZFCommands<cr>',   silent = true, remap = false,  desc = 'fzf: commands' },
-        { '<leader>/',        ':FZFHistory/<cr>',   silent = true, remap = false,  desc = 'fzf: search history' },
-        { '<leader>fT',       ':FZFTags<cr>',       silent = true, remap = false,  desc = 'fzf: ctags' },
-        { '<leader>fm',       ':FZFMarks<cr>',      silent = true, remap = false,  desc = 'fzf: marks' },
-        { '<leader>ft',       find_tag,             silent = true, remap = false,  desc = 'fzf: find buffer tag' },
+	"junegunn/fzf.vim",
+	-- event = 'VeryLazy',
+	dependencies = { "stevearc/aerial.nvim", fzf[1] },
+	init = function()
+		vim.g.fzf_command_prefix = "FZF"
+		vim.g.fzf_history_dir = "~/.fzf-history"
+	end,
+	config = function() end,
+	cmd = { "FZF", "FZFFiles", "FZFGFiles", "FZFBuffers", "FZFRg", "FZFAg" },
+	keys = {
+		{ "<leader>ff", ":FZFFiles<cr>", silent = true, remap = false, desc = "fzf: files" },
+		{ "<leader>fg", ":FZFGFiles<cr>", silent = true, remap = false, desc = "fzf: git files" },
+		{ "<leader>fb", ":FZFBuffers<cr>", silent = true, remap = false, desc = "fzf: buffers" },
+		{ "<leader>fr", ":FZFRg<cr>", silent = true, remap = false, desc = "fzf: rg" },
+		{ "<leader>fa", ":FZFAg<cr>", silent = true, remap = false, desc = "fzf: ag" },
+		{
+			"<leader>fA",
+			function()
+				local cword = vim.fn.expand("<cword>")
+				vim.cmd(vim.g.fzf_command_prefix .. "Ag " .. cword .. "<cr>")
+			end,
+			silent = true,
+			remap = false,
+			desc = "fzf: ag cword",
+		},
+		{ "<leader>fh", ":FZFHistory<cr>", silent = true, remap = false, desc = "fzf: command history" },
+		{ "<leader>fw", ":FZFWindows<cr>", silent = true, remap = false, desc = "fzf: windows" },
+		{ "<leader>fc", ":FZFCommands<cr>", silent = true, remap = false, desc = "fzf: commands" },
+		{ "<leader>/", ":FZFHistory/<cr>", silent = true, remap = false, desc = "fzf: search history" },
+		{ "<leader>fT", ":FZFTags<cr>", silent = true, remap = false, desc = "fzf: ctags" },
+		{ "<leader>fm", ":FZFMarks<cr>", silent = true, remap = false, desc = "fzf: marks" },
+		{ "<leader>ft", find_tag, silent = true, remap = false, desc = "fzf: find buffer tag" },
 
-        { '<leader><leader>', '<plug>(fzf-maps-n)', mode = 'n',    silent = false, remap = true,                 desc = 'fzf: nmap' },
-        { '<leader><leader>', '<plug>(fzf-maps-o)', mode = 'o',    silent = false, remap = true,                 desc = 'fzf: omap' },
-        { '<leader><leader>', '<plug>(fzf-maps-x)', mode = 'x',    silent = false, remap = true,                 desc = 'fzf: xmap' },
-    },
-
+		{
+			"<leader><leader>",
+			"<plug>(fzf-maps-n)",
+			mode = "n",
+			silent = false,
+			remap = true,
+			desc = "fzf: nmap",
+		},
+		{
+			"<leader><leader>",
+			"<plug>(fzf-maps-o)",
+			mode = "o",
+			silent = false,
+			remap = true,
+			desc = "fzf: omap",
+		},
+		{
+			"<leader><leader>",
+			"<plug>(fzf-maps-x)",
+			mode = "x",
+			silent = false,
+			remap = true,
+			desc = "fzf: xmap",
+		},
+	},
 }
 
 local marks = {
-    'tenfyzhong/fzf-marks.vim',
-    dependencies = { fzf[1] },
-    config = function()
-        local group = vim.api.nvim_create_augroup('fzf_marks_local', {})
-        vim.api.nvim_create_autocmd('User', {
-            group = group,
-            pattern = 'FZFMarksCd',
-            callback = function()
-                local cwd = vim.fn.getcwd()
-                require('nvim-tree.api').tree.open { path = cwd }
-            end,
-        })
-    end,
-    keys = {
-        { '<leader>fs', ':FZFFzm<cr>', silent = true, remap = false, desc = 'fzf-marks: marks' },
-    },
-    cmd = { 'FZFFzm' },
+	"tenfyzhong/fzf-marks.vim",
+	dependencies = { fzf[1] },
+	config = function()
+		local group = vim.api.nvim_create_augroup("fzf_marks_local", {})
+		vim.api.nvim_create_autocmd("User", {
+			group = group,
+			pattern = "FZFMarksCd",
+			callback = function()
+				local cwd = vim.fn.getcwd()
+				require("nvim-tree.api").tree.open({ path = cwd })
+			end,
+		})
+	end,
+	keys = {
+		{ "<leader>fs", ":FZFFzm<cr>", silent = true, remap = false, desc = "fzf-marks: marks" },
+	},
+	cmd = { "FZFFzm" },
 }
 
 local bookmarks = {
-    'tenfyzhong/fzf-bookmarks.vim',
-    dependencies = { fzf[1] },
-    config = function()
-    end,
-    keys = {
-        { '<leader>fM', ':FZFBookmarks<cr>', silent = true, remap = false, desc = 'fzf-bookmarks: bookmarks' },
-    },
-    cmd = { 'FZFBookmarks' },
+	"tenfyzhong/fzf-bookmarks.vim",
+	dependencies = { fzf[1] },
+	config = function() end,
+	keys = {
+		{ "<leader>fM", ":FZFBookmarks<cr>", silent = true, remap = false, desc = "fzf-bookmarks: bookmarks" },
+	},
+	cmd = { "FZFBookmarks" },
 }
 
 local z = {
-    'tenfyzhong/z.nvim',
-    dependencies = { fzf[1] },
-    config = function()
-        require('z').setup {}
+	"tenfyzhong/z.nvim",
+	dependencies = { fzf[1] },
+	config = function()
+		require("z").setup({})
 
-        local group = vim.api.nvim_create_augroup('z_local', {})
-        vim.api.nvim_create_autocmd('User', {
-            group = group,
-            pattern = 'Zcd',
-            callback = function()
-                local cwd = vim.fn.getcwd()
-                require('nvim-tree.api').tree.open { path = cwd }
-            end,
-        })
-    end,
-    keys = {
-        { '<leader>fz', ':FZFZ<cr>', silent = true, remap = false, desc = 'z: z' },
-    },
-    cmd = { 'FZFZ' },
+		local group = vim.api.nvim_create_augroup("z_local", {})
+		vim.api.nvim_create_autocmd("User", {
+			group = group,
+			pattern = "Zcd",
+			callback = function()
+				local cwd = vim.fn.getcwd()
+				require("nvim-tree.api").tree.open({ path = cwd })
+			end,
+		})
+	end,
+	keys = {
+		{ "<leader>fz", ":FZFZ<cr>", silent = true, remap = false, desc = "z: z" },
+	},
+	cmd = { "FZFZ" },
 }
 
-return { fzf, fzf_vim, marks, bookmarks, z }
+return { fzf, fzf_lua, marks, bookmarks }
