be8d1f3630ddd2672361a122bf9a564c02f62638
tenfyzhong <tenfy@tenfy.cn>
2025-09-10 23:45:04 +0800

    refactor(config): Standardize string quotes and update shortmess option
    - Standardize string literals to use double quotes for consistency.
    - Add parentheses around table constructors passed as arguments for clarity.
    - Update `shortmess` option to include `o`, `O`, `t`, `F` flags for improved message display.
    
    Signed-off-by: tenfyzhong <tenfy@tenfy.cn>

--- lua/opt.lua
+++ lua/opt.lua
@@ -6,29 +6,29 @@
 - @created 2023-01-26 22:09:41
 --]]
 
-vim.o.shell = 'sh'
+vim.o.shell = "sh"
 
-vim.opt.clipboard:append { 'unnamedplus' }
+vim.opt.clipboard:append({ "unnamedplus" })
 
-vim.o.directory = '.'
+vim.o.directory = "."
 vim.o.compatible = false
 vim.o.cindent = true
-vim.opt.cinoptions:append { ':0', 'l1', 'g0', 'N-s', '(0', 'w1', 'W4', 'j1', 'J1' }
+vim.opt.cinoptions:append({ ":0", "l1", "g0", "N-s", "(0", "w1", "W4", "j1", "J1" })
 vim.o.foldlevelstart = 99
-vim.o.encoding = 'utf-8'
-vim.o.fileencoding = 'utf-8'
-vim.opt.fileencodings = { 'utf-8', 'gbk', 'gb2312', 'cp936', 'usc-bom', 'euc-jp', 'gb18030' }
-vim.o.mouse = ''
+vim.o.encoding = "utf-8"
+vim.o.fileencoding = "utf-8"
+vim.opt.fileencodings = { "utf-8", "gbk", "gb2312", "cp936", "usc-bom", "euc-jp", "gb18030" }
+vim.o.mouse = ""
 vim.o.wrap = true
 vim.o.wrapmargin = 0
 vim.o.textwidth = 79
 vim.o.colorcolumn = "120"
-vim.opt.backspace:append { 'indent', 'eol', 'start' }
-vim.o.whichwrap = 'b,s,<,>,h,l'
-vim.o.formatoptions = 'tcrqn'
+vim.opt.backspace:append({ "indent", "eol", "start" })
+vim.o.whichwrap = "b,s,<,>,h,l"
+vim.o.formatoptions = "tcrqn"
 vim.o.autowrite = true
 vim.o.scrolloff = 2
-vim.o.shortmess = 'atIcA'
+vim.o.shortmess = "atIcAoOtF"
 vim.o.autoread = true
 vim.o.cmdheight = 1
 vim.o.hidden = true
@@ -50,18 +50,18 @@ vim.o.showtabline = 0
 vim.o.laststatus = 2
 vim.o.number = true
 vim.o.relativenumber = true
-vim.opt.completeopt = { 'menu', 'menuone', 'noselect', 'noinsert' }
+vim.opt.completeopt = { "menu", "menuone", "noselect", "noinsert" }
 vim.o.splitright = true
-vim.opt.diffopt = { 'filler', 'vertical' }
-vim.opt.shada:append { "'0" }
-vim.o.belloff = 'all'
+vim.opt.diffopt = { "filler", "vertical" }
+vim.opt.shada:append({ "'0" })
+vim.o.belloff = "all"
 vim.o.errorbells = false
 vim.o.visualbell = false
-vim.opt.sessionoptions:remove { 'buffers' }
+vim.opt.sessionoptions:remove({ "buffers" })
 vim.o.updatetime = 200
 vim.opt.termguicolors = true
 vim.o.undofile = true
 
-vim.o.background = 'dark'
+vim.o.background = "dark"
 
 vim.diagnostic.config({ virtual_text = true })
