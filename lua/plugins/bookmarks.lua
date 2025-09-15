dd1b37fba49930c3f8f1d61fc75d4b53457037a6
tenfyzhong <tenfy@tenfy.cn>
2025-09-16 00:10:24 +0800

    feat(bookmarks): Pre-fill annotation with word under cursor
    - The `Ma` keybinding for `bookmark_ann` now automatically populates the annotation prompt with the word under the cursor.
    - This streamlines the process of adding annotations by reducing manual input.
    
    Signed-off-by: tenfyzhong <tenfy@tenfy.cn>

--- lua/plugins/bookmarks.lua
+++ lua/plugins/bookmarks.lua
@@ -5,7 +5,10 @@ local bookmarks = {
         bm.setup({})
         local map = vim.keymap.set
         map("n", "Mm", bm.bookmark_toggle) -- add or remove bookmark at current line
-        map("n", "Ma", bm.bookmark_ann) -- add or edit mark annotation at current line
+        map("n", "Ma", function()
+            local word = vim.fn.expand("<cword>")
+            bm.bookmark_ann(word)
+        end) -- add or edit mark annotation at current line
         map("n", "Mc", bm.bookmark_clean) -- clean all marks in local buffer
         map("n", "Mn", bm.bookmark_next) -- jump to next mark in local buffer
         map("n", "Mp", bm.bookmark_prev) -- jump to previous mark in local buffer
