cf5b88b6b9eefdfdca32f7e0bf2825fe540df351
tenfyzhong <tenfy@tenfy.cn>
2025-09-18 00:07:25 +0800

    refactor(nvim-cmp): Standardize string quotes and enhance buffer source
    - Standardized the use of double quotes for string literals across the nvim-cmp configuration to improve consistency.
    - Configured the `buffer` completion source with a `keyword_pattern` to improve word matching and suggestions.
    
    Signed-off-by: tenfyzhong <tenfy@tenfy.cn>

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -7,7 +7,7 @@
 --]]
 
 local function cmp_config()
-    local cmp = require 'cmp'
+    local cmp = require("cmp")
 
     local has_words_before = function()
         unpack = unpack or table.unpack
@@ -69,7 +69,7 @@ local function cmp_config()
         end
     end, { "i", "s" })
 
-    local lspkind = require('lspkind')
+    local lspkind = require("lspkind")
 
     cmp.setup({
         sorting = {
@@ -107,39 +107,44 @@ local function cmp_config()
             documentation = cmp.config.window.bordered(),
         },
         mapping = {
-            ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-            ['<C-f>'] = cmp.mapping.scroll_docs(4),
+            ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+            ["<C-f>"] = cmp.mapping.scroll_docs(4),
             -- ['<C-d>'] = cmp.mapping.complete(),
-            ['<C-e>'] = abort,
-            ['<CR>'] = cmp.mapping.confirm({ select = false, behavior = cmp.ConfirmBehavior.Insert }),
-            ['<c-n>'] = cmp.mapping.select_next_item(),
-            ['<c-p>'] = cmp.mapping.select_prev_item(),
-            ['<Tab>'] = super_next,
-            ['<S-Tab>'] = super_prev,
-            ['<c-j>'] = snip_jump_next,
-            ['<c-k>'] = snip_jump_prev,
-            ['<Down>'] = super_next,
-            ['<Up>'] = super_prev,
+            ["<C-e>"] = abort,
+            ["<CR>"] = cmp.mapping.confirm({ select = false, behavior = cmp.ConfirmBehavior.Insert }),
+            ["<c-n>"] = cmp.mapping.select_next_item(),
+            ["<c-p>"] = cmp.mapping.select_prev_item(),
+            ["<Tab>"] = super_next,
+            ["<S-Tab>"] = super_prev,
+            ["<c-j>"] = snip_jump_next,
+            ["<c-k>"] = snip_jump_prev,
+            ["<Down>"] = super_next,
+            ["<Up>"] = super_prev,
         },
         sources = cmp.config.sources({
-            { name = 'nvim_lsp' },
-            { name = 'nvim_lsp_signature_help' },
-            { name = 'nvim_lua' },
-            { name = 'vsnip' }, -- For vsnip users.
+            { name = "nvim_lsp" },
+            { name = "nvim_lsp_signature_help" },
+            { name = "nvim_lua" },
+            { name = "vsnip" }, -- For vsnip users.
             -- { name = 'crates' },
             { name = "codeium" },
             -- { name = 'luasnip' }, -- For luasnip users.
             -- { name = 'ultisnips' }, -- For ultisnips users.
             -- { name = 'snippy' }, -- For snippy users.
         }, {
-            { name = 'buffer' },
+            {
+                name = "buffer",
+                option = {
+                    keyword_pattern = [[\k\+]],
+                },
+            },
         }),
         formatting = {
             format = lspkind.cmp_format({
-                mode = 'symbol',       -- show only symbol annotations
-                maxwidth = 50,         -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-                ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-                symbol_map = { Codeium = "", },
+                mode = "symbol", -- show only symbol annotations
+                maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+                ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+                symbol_map = { Codeium = "" },
 
                 -- The function below will be called before any actual modifications from lspkind
                 -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
@@ -147,18 +152,18 @@ local function cmp_config()
                     -- ...
                     return vim_item
                 end,
-            })
+            }),
         },
         preselect = cmp.PreselectMode.None,
     })
 
     -- Set configuration for specific filetype.
-    cmp.setup.filetype('gitcommit', {
+    cmp.setup.filetype("gitcommit", {
         sources = cmp.config.sources({
-            { name = 'git' }, -- You can specify the `cmp_git` source if you were installed it.
-            { name = 'vsnip' },
+            { name = "git" }, -- You can specify the `cmp_git` source if you were installed it.
+            { name = "vsnip" },
         }, {
-            { name = 'buffer' },
+            { name = "buffer" },
         }),
         sorting = {
             comparators = {
@@ -174,42 +179,41 @@ local function cmp_config()
         },
     })
 
-    cmp.setup.filetype('cargo.toml', {
+    cmp.setup.filetype("cargo.toml", {
         sources = cmp.config.sources({
-            { name = 'crates' },
-            { name = 'vsnip' },
-        })
+            { name = "crates" },
+            { name = "vsnip" },
+        }),
     })
 
     -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-    cmp.setup.cmdline({ '/', '?' }, {
+    cmp.setup.cmdline({ "/", "?" }, {
         mapping = cmp.mapping.preset.cmdline(),
         sources = {
-            { name = 'buffer' }
-        }
+            { name = "buffer" },
+        },
     })
 
     -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-    cmp.setup.cmdline(':', {
+    cmp.setup.cmdline(":", {
         mapping = cmp.mapping.preset.cmdline(),
         sources = cmp.config.sources({
-            { name = 'path' }
+            { name = "path" },
         }, {
-            { name = 'cmdline' }
-        })
+            { name = "cmdline" },
+        }, {
+            { name = "buffer" },
+        }),
     })
 
-    local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-    cmp.event:on(
-        'confirm_done',
-        cmp_autopairs.on_confirm_done()
-    )
+    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+    cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 
     require("neodev").setup({})
 end
 
 local function lspkind_config()
-    require('lspkind').init({
+    require("lspkind").init({
         -- DEPRECATED (use mode instead): enables text annotations
         --
         -- default: true
@@ -218,14 +222,14 @@ local function lspkind_config()
         -- defines how annotations are shown
         -- default: symbol
         -- options: 'text', 'text_symbol', 'symbol_text', 'symbol'
-        mode = 'symbol_text',
+        mode = "symbol_text",
 
         -- default symbol map
         -- can be either 'default' (requires nerd-fonts font) or
         -- 'codicons' for codicon preset (requires vscode-codicons font)
         --
         -- default: 'default'
-        preset = 'codicons',
+        preset = "codicons",
 
         -- override preset symbols
         --
@@ -260,36 +264,36 @@ local function lspkind_config()
     })
 end
 
-local cmp_nvim_lsp = { 'hrsh7th/cmp-nvim-lsp' }
-local cmp_buffer = { 'hrsh7th/cmp-buffer' }
-local cmp_path = { 'hrsh7th/cmp-path' }
-local cmp_cmdline = { 'hrsh7th/cmp-cmdline' }
-local cmp_vsnip = { 'hrsh7th/cmp-vsnip' }
-local lspkind = { 'onsails/lspkind.nvim', config = lspkind_config }
+local cmp_nvim_lsp = { "hrsh7th/cmp-nvim-lsp" }
+local cmp_buffer = { "hrsh7th/cmp-buffer" }
+local cmp_path = { "hrsh7th/cmp-path" }
+local cmp_cmdline = { "hrsh7th/cmp-cmdline" }
+local cmp_vsnip = { "hrsh7th/cmp-vsnip" }
+local lspkind = { "onsails/lspkind.nvim", config = lspkind_config }
 
-local cmp_nvim_lua = { 'hrsh7th/cmp-nvim-lua', ft = 'lua' }
+local cmp_nvim_lua = { "hrsh7th/cmp-nvim-lua", ft = "lua" }
 
-local tsnippets = { 'tenfyzhong/tsnippets.vim' }
-local friendly_snippets = { 'rafamadriz/friendly-snippets' }
+local tsnippets = { "tenfyzhong/tsnippets.vim" }
+local friendly_snippets = { "rafamadriz/friendly-snippets" }
 
-local cmp_nvim_lsp_signature_help = { 'hrsh7th/cmp-nvim-lsp-signature-help' }
+local cmp_nvim_lsp_signature_help = { "hrsh7th/cmp-nvim-lsp-signature-help" }
 
-local neodev = { 'folke/neodev.nvim', ft = 'lua' }
+local neodev = { "folke/neodev.nvim", ft = "lua" }
 -- local plenary = { 'nvim-lua/plenary.nvim' }
 --
 local vim_vsnip = {
-    'hrsh7th/vim-vsnip',
+    "hrsh7th/vim-vsnip",
     init = function()
         vim.g.vsnip_filetypes = {
-            ['envrc.sh'] = { 'sh', 'bash', 'envrc' },
-            ['sh'] = { 'sh', 'bash' },
+            ["envrc.sh"] = { "sh", "bash", "envrc" },
+            ["sh"] = { "sh", "bash" },
         }
-    end
+    end,
 }
 
 local cmp_git = {
-    'petertriho/cmp-git',
-    ft = { 'gitcommit', 'octo' },
+    "petertriho/cmp-git",
+    ft = { "gitcommit", "octo" },
     config = function()
         local format = require("cmp_git.format")
         local sort = require("cmp_git.sort")
@@ -297,20 +301,20 @@ local cmp_git = {
             label = format.git.commits.label,
             filterText = format.git.commits.filterText,
             insertText = function(trigger_char, commit)
-                return (commit.description == nil or commit.description == '') and commit.title or
-                    string.format("%s\n\n%s", commit.title, commit.description)
+                return (commit.description == nil or commit.description == "") and commit.title
+                    or string.format("%s\n\n%s", commit.title, commit.description)
             end,
             documentation = format.git.commits.documentation,
         }
 
-        local github_hosts = { 'github.com' }
-        local github_private = os.getenv('GITHUB_PRIVATE_HOST')
+        local github_hosts = { "github.com" }
+        local github_private = os.getenv("GITHUB_PRIVATE_HOST")
         if github_private then
             github_hosts[#github_hosts + 1] = github_private
         end
 
-        local gitlab_hosts = { 'gitlab.com' }
-        local gitlab_private = os.getenv('GITLAB_PRIVATE_HOST')
+        local gitlab_hosts = { "gitlab.com" }
+        local gitlab_private = os.getenv("GITLAB_PRIVATE_HOST")
         if gitlab_private then
             gitlab_hosts[#gitlab_hosts + 1] = gitlab_private
         end
@@ -319,7 +323,7 @@ local cmp_git = {
             -- defaults
             filetypes = { "gitcommit", "octo" },
             remotes = { "origin", "upstream" }, -- in order of most to least prioritized
-            enableRemoteUrlRewrites = true,     -- enable git url rewrites, see https://git-scm.com/docs/git-config#Documentation/git-config.txt-urlltbasegtinsteadOf
+            enableRemoteUrlRewrites = true, -- enable git url rewrites, see https://git-scm.com/docs/git-config#Documentation/git-config.txt-urlltbasegtinsteadOf
             git = {
                 commits = {
                     limit = 100,
@@ -415,17 +419,16 @@ local cmp_git = {
                     end,
                 },
             },
-        }
-        )
+        })
     end,
 }
 
 local crates = {
     "saecki/crates.nvim",
-    tag = 'stable',
+    tag = "stable",
     event = { "BufRead Cargo.toml" },
     config = function()
-        require('crates').setup()
+        require("crates").setup()
     end,
 }
 
@@ -436,13 +439,12 @@ local codeium = {
         "hrsh7th/nvim-cmp",
     },
     config = function()
-        require("codeium").setup({
-        })
-    end
+        require("codeium").setup({})
+    end,
 }
 
 local nvim_cmp = {
-    'hrsh7th/nvim-cmp',
+    "hrsh7th/nvim-cmp",
     config = cmp_config,
     dependencies = {
         cmp_nvim_lsp,
@@ -460,7 +462,7 @@ local nvim_cmp = {
         crates,
         -- codeium,
     },
-    event = { 'InsertEnter', 'CmdlineEnter' },
+    event = { "InsertEnter", "CmdlineEnter" },
 }
 
 return {
