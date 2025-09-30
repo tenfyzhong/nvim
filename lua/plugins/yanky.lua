2d4ff31b7e2729c2ee0e30cc32293404ceecdcc9
tenfyzhong <tenfy@tenfy.cn>
2025-09-30 22:04:14 +0800

    feat(yanky): Configure yanky plugin
    - Replaced `config` function with `opts` for configuration.
    - Enabled shada storage for the yank ring.
    - Updated keybindings for yanky plugin to align with common Neovim practices and include more functionalities.
    
    Signed-off-by: tenfyzhong <tenfy@tenfy.cn>

--- lua/plugins/yanky.lua
+++ lua/plugins/yanky.lua
@@ -1,57 +1,33 @@
 local yanky = {
     "gbprod/yanky.nvim",
-    config = function()
-        require("yanky").setup({})
-    end,
+    -- config = function()
+    --     require("yanky").setup({})
+    -- end,
+    opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+        ring = { storage = "shada" },
+    },
     keys = {
-        {
-            "p",
-            "<Plug>(YankyPutAfter)",
-            mode = { "n", "x" },
-            silent = true,
-            remap = true,
-            desc = "yanky: yank put after",
-        },
-        {
-            "P",
-            "<Plug>(YankyPutBefore)",
-            mode = { "n", "x" },
-            silent = true,
-            remap = true,
-            desc = "yanky: yank put before",
-        },
-        {
-            "gp",
-            "<Plug>(YankyGPutAfter)",
-            mode = { "n", "x" },
-            silent = true,
-            remap = true,
-            desc = "yanky: yank gput after",
-        },
-        {
-            "gP",
-            "<Plug>(YankyGPutBefore)",
-            mode = { "n", "x" },
-            silent = true,
-            remap = true,
-            desc = "yanky: yank gput after",
-        },
-        {
-            "'yn",
-            "<Plug>(YankyCycleForward)",
-            mode = { "n" },
-            silent = true,
-            remap = true,
-            desc = "yanky: cycle forward",
-        },
-        {
-            "'yp",
-            "<Plug>(YankyCycleBackward)",
-            mode = { "n" },
-            silent = true,
-            remap = true,
-            desc = "yanky: cycle backward",
-        },
+        { "<leader>p", "<cmd>YankyRingHistory<cr>", mode = { "n", "x" }, desc = "Open Yank History" },
+        { "y", "<Plug>(YankyYank)", mode = { "n", "x" }, desc = "Yank text" },
+        { "p", "<Plug>(YankyPutAfter)", mode = { "n", "x" }, desc = "Put yanked text after cursor" },
+        { "P", "<Plug>(YankyPutBefore)", mode = { "n", "x" }, desc = "Put yanked text before cursor" },
+        { "gp", "<Plug>(YankyGPutAfter)", mode = { "n", "x" }, desc = "Put yanked text after selection" },
+        { "gP", "<Plug>(YankyGPutBefore)", mode = { "n", "x" }, desc = "Put yanked text before selection" },
+        { "<leader>yp", "<Plug>(YankyPreviousEntry)", desc = "Select previous entry through yank history" },
+        { "<leader>yn", "<Plug>(YankyNextEntry)", desc = "Select next entry through yank history" },
+        { "]p", "<Plug>(YankyPutIndentAfterLinewise)", desc = "Put indented after cursor (linewise)" },
+        { "[p", "<Plug>(YankyPutIndentBeforeLinewise)", desc = "Put indented before cursor (linewise)" },
+        { "]P", "<Plug>(YankyPutIndentAfterLinewise)", desc = "Put indented after cursor (linewise)" },
+        { "[P", "<Plug>(YankyPutIndentBeforeLinewise)", desc = "Put indented before cursor (linewise)" },
+        { ">p", "<Plug>(YankyPutIndentAfterShiftRight)", desc = "Put and indent right" },
+        { "<p", "<Plug>(YankyPutIndentAfterShiftLeft)", desc = "Put and indent left" },
+        { ">P", "<Plug>(YankyPutIndentBeforeShiftRight)", desc = "Put before and indent right" },
+        { "<P", "<Plug>(YankyPutIndentBeforeShiftLeft)", desc = "Put before and indent left" },
+        { "=p", "<Plug>(YankyPutAfterFilter)", desc = "Put after applying a filter" },
+        { "=P", "<Plug>(YankyPutBeforeFilter)", desc = "Put before applying a filter" },
     },
 }
 
