9946e1718ddf0c05221ffbafd34f684c6368e3aa
tenfyzhong <tenfy@tenfy.cn>
2025-10-02 12:01:10 +0800

    feat(dap): Configure nvim-dap and nvim-dap-ui
    - Add nvim-dap plugin configuration.
    - Configure nvim-dap-ui to open and close automatically with debugger events.
    - Ensure dependencies are correctly set for both plugins.
    
    Signed-off-by: tenfyzhong <tenfy@tenfy.cn>

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -1,25 +1,28 @@
+local dap = {
+    "mfussenegger/nvim-dap",
+    event = "VeryLazy",
+}
+
 local ui = {
     "rcarriga/nvim-dap-ui",
+    dependencies = { dap, "nvim-neotest/nvim-nio" },
+    event = "VeryLazy",
     config = function()
         local dap = require("dap")
         local dapui = require("dapui")
-        dapui.setup({})
-        dap.listeners.after.event_initialized["dapui_config"] = function()
+        dap.listeners.before.attach.dapui_config = function()
             dapui.open()
         end
-        dap.listeners.before.event_terminated["dapui_config"] = function()
+        dap.listeners.before.launch.dapui_config = function()
+            dapui.open()
+        end
+        dap.listeners.before.event_terminated.dapui_config = function()
             dapui.close()
         end
-        dap.listeners.before.event_exited["dapui_config"] = function()
+        dap.listeners.before.event_exited.dapui_config = function()
             dapui.close()
         end
     end,
 }
 
-local dap = {
-    "mfussenegger/nvim-dap",
-    event = "VeryLazy",
-    dependencies = { ui, "nvim-neotest/nvim-nio" },
-}
-
-return { dap }
+return { dap, ui }
