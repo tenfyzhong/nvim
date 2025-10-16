1be912cf56bb7df1e3bc443e40ee211f31dcda7b
tenfyzhong <tenfy@tenfy.cn>
2025-10-16 20:52:39 +0800

    feat(diffview): Configure diffview plugin
    - Change merge tool layout to `diff3_horizontal` for better visualization.
    - Add hooks to configure local options for diff view buffers:
        - Disable line wrapping (`wrap = false`).
        - Hide non-printable characters (`list = false`).
        - Set color column to 80 characters (`colorcolumn = { 80 }`).
        - Disable swap files (`swapfile = false`).
        - Set buffer hidden option to delete on close (`bufhidden = "delete"`).
    
    Signed-off-by: tenfyzhong <tenfy@tenfy.cn>

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -6,10 +6,20 @@ local diffview = {
             view = {
                 merge_tool = {
                     -- Config for conflicted files in diff views during a merge or rebase.
-                    layout = "diff3_mixed",
+                    layout = "diff3_horizontal",
                     disable_diagnostics = true, -- Temporarily disable diagnostics for conflict buffers while in the view.
                     winbar_info = true, -- See |diffview-config-view.x.winbar_info|
                 },
+            },
+            hooks = {
+                view_opened = function(bufnr)
+                    -- Change local options in diff buffers
+                    vim.opt.wrap = false
+                    vim.opt.list = false
+                    vim.opt.colorcolumn = { 80 }
+                    vim.opt.swapfile = false
+                    vim.opt.bufhidden = "delete"
+                end,
             },
         })
     end,
