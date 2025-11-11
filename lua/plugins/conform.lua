96826e2f1701aed4e68a509acbf71f54135ad1e9
tenfyzhong <tenfy@tenfy.cn>
2025-11-11 23:41:51 +0800

    feat(conform): Add shfmt formatter
    - Integrate `shfmt` as a new formatter option for shell scripts.
    - This allows for consistent formatting of shell scripts using `shfmt`.
    
    Signed-off-by: tenfyzhong <tenfy@tenfy.cn>

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -154,6 +154,9 @@ local conform = {
             log_level = vim.log.levels.TRACE,
             formatters_by_ft = formatters_by_ft,
             formatters = {
+                shfmt = gen_formatter("shfmt", {
+                    command = "shfmt",
+                }),
                 gofumpt = gen_formatter("gofumpt", {
                     command = "gofumpt",
                 }),
