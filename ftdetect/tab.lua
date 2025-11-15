11cab8af04960bb30ed48f512d62a5161faafafa
tenfyzhong <tenfy@tenfy.cn>
2025-11-16 01:28:11 +0800

    feat(tab): Improve tab settings configuration
    - Refactor tab settings to use environment variables for more flexible configuration.
    - Add support for overriding tab settings per filetype using `VIM_EXPANDTAB_<FILETYPE>` and `VIM_TABWIDTH_<FILETYPE>`.
    - Maintain default tab settings for common filetypes.
    - Simplify autocmd creation by using a single pattern for all filetypes.
    
    Signed-off-by: tenfyzhong <tenfy@tenfy.cn>

--- ftdetect/tab.lua
+++ ftdetect/tab.lua
@@ -1,32 +1,67 @@
-vim.api.nvim_create_autocmd({ "FileType" }, {
-    pattern = {
-        "css",
-        "ejs",
-        "html",
-        "htmldjango",
-        "htmljanja",
-        "jade",
-        "pug",
-        "javascript",
-        "typescript",
-        "vue",
-        "yaml",
-    },
-    callback = function()
-        vim.bo.tabstop = 2
-        vim.bo.softtabstop = 2
-        vim.bo.shiftwidth = 2
-        vim.bo.expandtab = true
-    end,
-})
+local tab2 = {
+    "css",
+    "ejs",
+    "html",
+    "htmldjango",
+    "htmljanja",
+    "jade",
+    "pug",
+    "javascript",
+    "typescript",
+    "vue",
+    "yaml",
+}
+
+local noexpand = {
+    "make",
+    "go",
+}
+
+local function contains(tab, val)
+    for _, value in ipairs(tab) do
+        if value == val then
+            return true
+        end
+    end
+    return false
+end
+
+local function env_tab(ft)
+    local ft_upper = ft:upper()
+    local res = false
+
+    local expandtab = os.getenv("VIM_EXPANDTAB_" .. ft_upper)
+    if expandtab and expandtab == "0" then
+        vim.bo.expandtab = false
+        res = true
+    end
+
+    local tabwidth = os.getenv("VIM_TABWIDTH_" .. ft_upper)
+    if tabwidth and tabwidth ~= "" then
+        local width = tonumber(tabwidth)
+        vim.bo.tabstop = width
+        vim.bo.softtabstop = width
+        vim.bo.shiftwidth = width
+        res = true
+    end
+
+    return res
+end
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
-    pattern = {
-        "make",
-        "go",
-        "java",
-    },
+    pattern = "*",
     callback = function()
-        vim.bo.expandtab = false
+        local ft = vim.bo.filetype
+
+        if env_tab(ft) then
+            env_tab(ft)
+        elseif contains(tab2, ft) then
+            vim.bo.tabstop = 2
+            vim.bo.softtabstop = 2
+            vim.bo.shiftwidth = 2
+            vim.bo.expandtab = true
+        elseif contains(noexpand, ft) then
+            vim.bo.expandtab = false
+        end
     end,
 })
