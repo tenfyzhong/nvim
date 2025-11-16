93665bd84f27cc15c77fafd7e03bed569a39fd49
tenfyzhong <tenfy@tenfy.cn>
2025-11-17 00:34:20 +0800

    feat(ftemplate): Add 'vim' to ftemplate ignore list
    - Include 'vim' filetype in the `ftemplate_ignore_ft` variable.
    - This prevents ftemplate from creating template files for Neovim configuration files,
      keeping the configuration directory cleaner.
    
    Signed-off-by: tenfyzhong <tenfy@tenfy.cn>

--- lua/plugins/ftemplate.lua
+++ lua/plugins/ftemplate.lua
@@ -2,7 +2,7 @@ local ftemplate = {
     "tenfyzhong/ftemplate.vim",
     init = function()
         vim.g.ftemplate_local_templates = "~/.config/nvim/local/ftemplates"
-        vim.g.ftemplate_ignore_ft = { "lua" }
+        vim.g.ftemplate_ignore_ft = { "lua", "vim" }
     end,
     config = function() end,
 }
