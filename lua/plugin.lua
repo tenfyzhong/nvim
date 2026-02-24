36f3e2be300b986c36af439aeb7b8e92dc7472a1
tenfyzhong <tenfy@tenfy.cn>
2026-02-25 00:34:46 +0800

    feat(plugin): add lazy.nvim auto-installation
    - Check for lazy.nvim existence before requiring
    - Clone lazy.nvim from stable branch if not present
    - Handle git clone errors with user-friendly messages
    - Exit with error code on installation failure
    
    Signed-off-by: tenfyzhong <tenfy@tenfy.cn>

--- lua/plugin.lua
+++ lua/plugin.lua
@@ -1,4 +1,22 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local uv = vim.uv or vim.loop
+if not uv.fs_stat(lazypath) then
+    local out = vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "--branch=stable",
+        "https://github.com/folke/lazy.nvim.git",
+        lazypath,
+    })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+        }, true, {})
+        os.exit(1)
+    end
+end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup("plugins", {
     defaults = {
