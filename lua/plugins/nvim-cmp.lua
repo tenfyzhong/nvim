ee7d0880b805284b6781dc77fc187f1f8e778fb1
tenfyzhong <tenfy@tenfy.cn>
2026-02-25 00:37:55 +0800

    feat(nvim-cmp): Improve compatibility with older Lua versions
    - Define `unpack_fn` to handle both `table.unpack` and `unpack`
    - Use the unified unpack function in `has_words_before` for better cross-version compatibility
    
    Signed-off-by: tenfyzhong <tenfy@tenfy.cn>

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,9 +1,9 @@
 local function cmp_config()
     local cmp = require("cmp")
+    local unpack_fn = table.unpack or unpack
 
     local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        local line, col = unpack_fn(vim.api.nvim_win_get_cursor(0))
         return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
     end
 
